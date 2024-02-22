0517743a62c4482ca62ecf1e548965c3d1fe0d1b
udK01 <dominik.muranyi@gmail.com>
2024-02-22 14:07:44 +0000

    Basic HTML and Messy CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  let balance;
+  let income;
+  let expense;
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div className="expense-tracker-container">
+        <h2 className="expense-tracker-header">Expense Tracker</h2>
 
-export default App
+        <div className="balance-container">
+          <h4>YOUR BALANCE</h4>
+          <h4 className="balance-display">£0.00</h4>
+        </div>
+
+        <div className="income-expense-display">
+          <div>
+            <label>INCOME</label>
+            <p className="income-text">£0.00</p>
+          </div>
+          <div>
+            <label>EXPENSE</label>
+            <p className="expense-text">£0.00</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
